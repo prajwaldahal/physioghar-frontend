96c1ef56cecfbfd70cd748f8a13737e88f0ee027
Prajwal Dahal <prajwaldahal072@gmail.com>
2026-09-20 18:53:10 +0545

    Bundle app fonts and turn off runtime fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/theme/app_theme.dart';
 
 void main() {
-  runApp(const MainApp());
+  // Every weight used by AppTextStyles ships in assets/google_fonts.
+  GoogleFonts.config.allowRuntimeFetching = false;
+  runApp(const ProviderScope(child: PhysioGharApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class PhysioGharApp extends StatelessWidget {
+  const PhysioGharApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'PhysioGhar',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: const Scaffold(),
     );
   }
 }
